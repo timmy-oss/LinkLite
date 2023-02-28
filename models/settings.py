ce29210e4c8b77f2c68723a47d6d458c7a1a8f12
timmy-oss <timileyinpelumi.dev@gmail.com>
2023-02-28 04:45:55 +0100

    fixes and UI updates

--- models/settings.py
+++ models/settings.py
@@ -1,18 +1,11 @@
-from pydantic import BaseSettings
-from pydantic import AnyUrl
-from typing import Any
-
-
-
-
-
+from pydantic import BaseSettings, AnyUrl
 
 
 class Settings(BaseSettings):
     " Model for the application settings  "
     debug : bool = True
     app_name : str = "LinkLite"
-    allowed_origins : list[str] = ["http://localhost:3000","https://timmy-oss-sturdy-bassoon-x47x4vqv4q536vjw-3000.preview.app.github.dev"]
+    allowed_origins : list[str] = ["http://localhost:3000",]
     db_url : AnyUrl = "redis://127.0.0.1:10005"
     db_username : str = "default"
     db_password  : str = "root"
